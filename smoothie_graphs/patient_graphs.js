1909184af961eef5a07dd80a599aa19bf10a9bb3
Peter Pokorny <pmpokorny@gmail.com>
2015-07-26 17:38:40 -0700

    (WEB-69) Recieve fake form alerts from server in debug mode. Change right info area to danger panel on alert. Count alerts and display number in info panel heading. Also refactor initial resizing to solve asynch conflicts.

--- smoothie_graphs/patient_graphs.js
+++ smoothie_graphs/patient_graphs.js
@@ -3,17 +3,20 @@
 // for the ecg graphs on the patient graphs.
 var currentGraphs = [];
 var graphOff = 0;
+var patient_id;
+var count_alert = ( function() {
+    var alertcount = 0;
+    return function() {return alertcount += 1;}
+})();
 
 function detail_graphs(eb) {
     var currentBuffers = {};
     var neededGraphs = [];
-    neededGraphs.push(['waveform', 'bp', 1]);
-    neededGraphs.push(['waveform', 'bp', 2]);
-    neededGraphs.push(['waveform', 'bp', 3]);
-    neededGraphs.push(['waveform', 'bp', 4]);
-
-    console.log("hi");
-    console.log(neededGraphs);
+    patient_id  = window.location.hash.substring(1);
+    neededGraphs.push(['waveform', 'ECG', 1]);
+    neededGraphs.push(['waveform', 'ABP', 2]);
+    neededGraphs.push(['waveform', 'PAP', 3]);
+    neededGraphs.push(['waveform', 'RESP', 4]);
 
     $("#graphs").click(function() {
         var egraph = document.getElementById("graphs");
@@ -38,11 +41,11 @@ function detail_graphs(eb) {
 
 
     var startGraph = function (stream, type, id) {
-        $.when($.ajax('http://api.s-pi-demo.com/stream/'+stream+'/'+type+'/'+id)).done(
+        $.when($.ajax('http://api.s-pi-demo.com/stream/'+stream+'/'+type+'/'+ patient_id)).done(
             function (data) {
                 var channelName = data;
                 var startTime = Date.now();
-                currentBuffers[channelName] = [];
+                currentBuffers[channelName] = new Array();
                 var chart = makeSmoothie('chart' + id);
                 currentGraphs.push({"channel": channelName,
                 "startTime": startTime,
@@ -52,6 +55,7 @@ function detail_graphs(eb) {
                 eb.registerHandler(channelName, function(msg) {
                     currentBuffers[channelName].push(msg.data);
                 });
+                handleResize(currentGraphs.length -1);
             }
         );
     };
@@ -67,7 +71,7 @@ function detail_graphs(eb) {
 
     var drawIt = function () {
         currentGraphs.forEach(function (item, idx, thisArray) {
-            var data = item.buffer.shift();
+            var data = item["buffer"].shift();
             if (typeof data !== 'undefined') {
                 for (var i = data.length - 1; i >= 0; i--) {
                     item.graph.append(item.startTime, data[i].SIGNAL);
@@ -78,20 +82,61 @@ function detail_graphs(eb) {
     };
 
     eb.onopen = function () {
-        var timer;
-        for (var i = neededGraphs.length - 1; i >= 0; i--) {
-            startGraph(neededGraphs[i][0], neededGraphs[i][1], neededGraphs[i][2]);
-        }
-        clearTimeout(timer);
-        timer = setTimeout(handleResize, 100);
-        setInterval(drawIt, 400);
+        neededGraphs.forEach(function(element){
+            startGraph(element[0], element[1], element[2]);
+        });
+        var graph_interval = setInterval(drawIt, 1000);
+
+        $.when($.ajax("http://api.s-pi-demo.com/alerts/" + patient_id )).done(get_alert);
     };
+
+    function get_alert(dat1, dat2, dat3) {
+            console.log('dat1: ' + dat1 + ' dat2: ' + dat2 + ' dat3: ' + dat3);
+            eb.registerHandler(dat1, function(msg) {
+                // temporary to test against debug mode data
+                make_alert(msg);
+                // Test this against live data
+                // array.data.forEach(makeAlert);
+            });
+    }
+    function make_alert(msg) {
+        console.log('in makeAlert');
+        var Alert = {};
+        Alert.alert_msg = msg.alert_msg; //ALERT_MSG;
+        Alert.action_msg = msg.action_msg; //ACTION_MSG;
+        Alert.alert_status = "Active Warning";
+        Alert.alert_time = new Date(msg.ts); //TS);
+        Alert.id = patient_id; //PATIENT_ID;
+        Alert.interval = msg.interval; //INTERVAL;
+        Alert.signame = msg.signame; //SIGNAME;
+
+
+        $.getJSON('/patients.json', function(data) {
+            Alert.name = data['patients'][(Alert.id)]['name'];
+            Alert.age =  data['patients'][(Alert.id)]['age'];
+            Alert.bed =  data['patients'][(Alert.id)]['bed'];
+            render_alert();
+
+        });
+        function render_alert(){
+            console.log('in render_alert');
+            $('#alertPanel').removeClass('panel-success').addClass('panel-danger');
+            $('#alertPanelTitle').text(count_alert() + ' Alerts');
+        }
+    }
 }
-var handleResize = function () {
-    for (var i = 0; i < currentGraphs.length; i++) {
-        console.log('resized' + i);
-        var mycanvas = currentGraphs[i].chart.canvas;
+var handleResize = function (graph_id) {
+    var mycanvas;
+    if (graph_id === undefined) {
+        for (var i = 0; i < currentGraphs.length; i++) {
+            mycanvas = currentGraphs[i].chart.canvas;
+            mycanvas.width = mycanvas.parentNode.offsetWidth;
+            currentGraphs[i].chart.resize();
+        }
+    } else {
+        console.log('in resize graph: ' + graph_id);
+        mycanvas = currentGraphs[graph_id].chart.canvas;
         mycanvas.width = mycanvas.parentNode.offsetWidth;
-        currentGraphs[i].chart.resize();
+        currentGraphs[graph_id].chart.resize();
     }
 };
